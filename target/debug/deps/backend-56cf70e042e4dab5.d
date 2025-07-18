dc23ea8512639785e92e83aa011e909efc987adf
altrix <altrix.solvers@gmail.com>
2025-07-18 11:45:24 -0400

    ignore target

--- target/debug/deps/backend-56cf70e042e4dab5.d
+++
@@ -1,5 +0,0 @@
-/Users/yutatoyomi/Work/crystal-peer/target/debug/deps/backend-56cf70e042e4dab5.d: backend/src/main.rs
-
-/Users/yutatoyomi/Work/crystal-peer/target/debug/deps/libbackend-56cf70e042e4dab5.rmeta: backend/src/main.rs
-
-backend/src/main.rs:
--- target/debug/deps/backend-89cabfa6e63a4cd4.d
+++
@@ -1,5 +0,0 @@
-/Users/yutatoyomi/Work/crystal-peer/target/debug/deps/backend-89cabfa6e63a4cd4.d: backend/src/main.rs
-
-/Users/yutatoyomi/Work/crystal-peer/target/debug/deps/libbackend-89cabfa6e63a4cd4.rmeta: backend/src/main.rs
-
-backend/src/main.rs:
