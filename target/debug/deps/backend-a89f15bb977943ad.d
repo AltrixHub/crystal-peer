1df87eead938fc9fda7da6457cfb165ac35f2c6e
altrix <altrix.solvers@gmail.com>
2025-07-18 11:49:09 -0400

    Remove target directory from Git tracking

--- target/debug/deps/backend-a89f15bb977943ad.d
+++
@@ -1,5 +0,0 @@
-/Users/yutatoyomi/Work/crystal-peer/target/debug/deps/backend-a89f15bb977943ad.d: backend/src/main.rs
-
-/Users/yutatoyomi/Work/crystal-peer/target/debug/deps/backend-a89f15bb977943ad: backend/src/main.rs
-
-backend/src/main.rs:
